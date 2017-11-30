f6e562cad2474874c42a444594eace13a33dceda
alexander grajales <alex-12-04@hotmail.com>
2017-11-29 23:33:55 -0500

    mejoras al diseño de usuarios

--- Usuario/static/usuario/usuario.directive.js
+++ Usuario/static/usuario/usuario.directive.js
@@ -8,16 +8,16 @@
         return {
             templateUrl: '/static/usuario/usuario.html', 
             restrict: 'E',
-            controller: ['$scope', '$http', function ($scope, $http){
-                console.log($scope.$$childHead);
-                // var url = '/apiusuario/usuario/'+$scope.usuario.id + '/';
-                // $scope.update = function(){
-                //     $http.put(url, $scope.usuario
-                //     );
-                // };
+            controller: ['$scope', '$http', function ($scope, $http){        
+                // console.log($scope)        
+                // var url = '/apiusuario/usuario/'+$scope.usuario.id + '/';                
+                //  $scope.update = function(){
+                //      $http.put(url, $scope.usuario
+                //      );
+                //  };
 
 
-                $scope.delete = function(){
+                $scope.delete = function(){                    
                     $http.delete(url).then(
                         function(){
                             var usuarios = $scope.list.usuarios;
--- Usuario/static/usuario/usuario.js
+++ Usuario/static/usuario/usuario.js
@@ -5,11 +5,11 @@
         .controller('UsuarioController', ['$scope', '$http', UsuarioController]);
 
     function UsuarioController($scope, $http) {
-        $scope.add = function (nombre, correo, clave, direccion) {
+        $scope.add = function (nombre, apellido, correo, clave, direccion, celular, telefono, edad) {
             
             var usuario = {
-                nombre: nombre,
-                correo: correo, clave:clave, direccion:direccion
+                nombre: nombre, apellido:apellido, 
+                correo: correo, clave:clave, direccion:direccion, celular:celular, telefono:telefono, edad:edad,
             };
             $http.post('/apiusuario/usuario/', usuario)
             .then(function (response) {                
@@ -22,8 +22,8 @@
 
         $scope.data = [];
         $http.get('/apiusuario/usuario/').then(function (response) {   
-            // console.log("get");  
-            // console.log(response.data);
+            //console.log();  
+            //console.log(response.data);
             $scope.data = response.data.results;
         });                
     }
